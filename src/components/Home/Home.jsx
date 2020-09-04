db04830b599febc4ffb17242e761164824038a0a
Serj <bondar54711745@gmail.com>
2020-09-04 17:03:20 +0300

    Fix styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,91 @@
-import React from "react"
+import React, { useState } from "react"
+import PropTypes from "prop-types"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import useScrollTrigger from "@material-ui/core/useScrollTrigger"
+import Box from "@material-ui/core/Box"
+import Container from "@material-ui/core/Container"
+import Slide from "@material-ui/core/Slide"
+import Brightness7Icon from "@material-ui/icons/Brightness7"
+import Brightness4Icon from "@material-ui/icons/Brightness4"
+import { deepPurple, deepOrange } from "@material-ui/core/colors"
+import { IconButton } from "@material-ui/core"
 
 import UploadResumeComponent from "../UploadResumeComponent"
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles"
+import { useStyles } from "./styles"
 
-export default function Home() {
+function HideOnScroll(props) {
+  const { children, window } = props
+  const trigger = useScrollTrigger({ target: window ? window() : undefined })
+
   return (
-    <div>
-      <UploadResumeComponent />
-    </div>
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
   )
+}
+
+export default function HideAppBar(props) {
+  const classes = useStyles()
+  const [darkState, setDarkState] = useState(false)
+  const palletType = darkState ? "dark" : "light"
+  const mainPrimaryColor = darkState ? "#424242" : "#2196f3"
+  const mainSecondaryColor = darkState ? deepOrange[900] : deepPurple[500]
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: palletType,
+      primary: {
+        main: mainPrimaryColor,
+      },
+      secondary: {
+        main: mainSecondaryColor,
+      },
+    },
+  })
+  const handleThemeChange = () => {
+    setDarkState(!darkState)
+  }
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <div className={classes.root}>
+          <AppBar>
+            <Toolbar>
+              <Typography variant="h6">RESUME BUILDER</Typography>
+              {darkState ? (
+                <IconButton
+                  onClick={handleThemeChange}
+                  className={classes.brightnessButton}
+                >
+                  <Brightness7Icon />
+                </IconButton>
+              ) : (
+                <IconButton
+                  onClick={handleThemeChange}
+                  className={classes.brightnessButton}
+                >
+                  <Brightness4Icon />
+                </IconButton>
+              )}
+            </Toolbar>
+          </AppBar>
+        </div>
+      </HideOnScroll>
+      <Toolbar />
+      <Container>
+        <Box my={2}>
+          <UploadResumeComponent />
+        </Box>
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  window: PropTypes.func,
 }
---
+++ src/components/Home/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles"
+
+export const useStyles = makeStyles(() => ({
+  appBar: {
+    flexGrow: 1,
+  },
+  brightnessButton: {
+    marginLeft: "75%",
+  },
+}))
