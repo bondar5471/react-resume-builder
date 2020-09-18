fcb62eb9a506685be9183cc91bfe468be53ec1b1
Serj <bondar54711745@gmail.com>
2020-09-18 16:39:04 +0300

    Add autocomplete tools

--- src/components/TechnologyStackForm/TechnologyStackForm.jsx
+++ src/components/TechnologyStackForm/TechnologyStackForm.jsx
@@ -6,12 +6,16 @@ import DialogActions from "@material-ui/core/DialogActions"
 import DialogContent from "@material-ui/core/DialogContent"
 import DialogTitle from "@material-ui/core/DialogTitle"
 import { TextField } from "@material-ui/core"
+import Autocomplete from "@material-ui/lab/Autocomplete"
+
+import { useStyles } from "./styles"
 
 export default function TechnologyStackForm({
   handleCloseTsForm,
   openTsForm,
   addTools,
 }) {
+  const classes = useStyles()
   const [category, setCategory] = useState("")
   const [stack, setStack] = useState("")
 
@@ -20,24 +24,45 @@ export default function TechnologyStackForm({
     addTools(tools)
     handleCloseTsForm()
   }
+
+  const options = [
+    "Programming Languages",
+    "Ruby",
+    "Testing Tools",
+    "Databases",
+    "JavaScript",
+    "Responsive design",
+    "CSS",
+    "Version Control Systems",
+    "Virtualization",
+    "Cloud Platforms",
+    "Tools",
+  ]
   return (
     <div>
       <Dialog
         open={openTsForm}
         onClose={handleCloseTsForm}
+        fullWidth
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
         <DialogTitle id="alert-dialog-title">{"Add new category"}</DialogTitle>
         <DialogContent>
-          <TextField
-            fullWidth
-            variant="filled"
-            label="Category"
-            onBlur={(e) => setCategory(e.target.value)}
+          <Autocomplete
+            className={classes.textField}
+            options={options.map((option) => option)}
+            onChange={(event, newValue) => {
+              setCategory(newValue)
+            }}
+            style={{ width: "100%" }}
+            renderInput={(params) => (
+              <TextField {...params} label="Category" variant="outlined" />
+            )}
           />
           <TextField
             fullWidth
+            className={classes.textField}
             variant="filled"
             label="Stack"
             onBlur={(e) => setStack(e.target.value)}
