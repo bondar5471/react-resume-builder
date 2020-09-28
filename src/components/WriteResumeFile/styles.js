c0060cae289000aadd3c05fc068b664a4e78100a
Serj <bondar54711745@gmail.com>
2020-09-28 17:28:50 +0300

    Add rename file

---
+++ src/components/WriteResumeFile/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core/styles"
+
+export const useStyles = makeStyles(() => ({
+  card: {
+    padding: "16px",
+    marginTop: "15px",
+  },
+  inputFileName: {
+    marginTop: "8px",
+  },
+}))
