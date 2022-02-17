f46b581eca08fdf3438ff8f0d22c83ad7afa462f
Artur Rozhanskyi <artur.rozhanskyi@nixsolutions.com>
2022-02-17 18:41:04 +0200

    [Feature] Add Keycloak Authentication

--- src/components/Router/PublicRoute.js
+++ src/components/Router/PublicRoute.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { isLogin } from '../../services/isLogin';
 import PropTypes from 'prop-types';
 
 const PublicRoute = ({ component: Component, restricted, ...rest }) => (
   <Route
     {...rest}
-    render={props => (isLogin() && restricted ? <Redirect to="/home" /> : <Component {...props} />)}
+    render={props => (restricted ? <Redirect to="/" /> : <Component {...props} />)}
   />
 );
 
---
+++ src/services/HandlerKeycloak.js
@@ -0,0 +1,14 @@
+import Keycloak from "keycloak-js";
+
+const keycloak = new Keycloak({
+ url: process.env.REACT_APP_KEYCLOAK_URL,
+ realm: process.env.REACT_APP_KEYCLOAK_REALM,
+ clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID,
+});
+
+export default keycloak;
+
+
+export const keycloakProviderConfig = {
+  onLoad: 'login-required'
+};
